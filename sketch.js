ba2a6c720777af2233da055c98f9031035566979
Andrew Hill <contact@andrewthomashill.co.uk>
2022-04-20 17:12:53 +0200

    Improve performance

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ const CAMERA_RES = 1920 / 1080;
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerWidth / CAMERA_RES;
-const LIFESPAN = 100;
+const LIFESPAN = 50;
 
 const activeStrokes = {};
 
@@ -74,23 +74,24 @@ const randomColor = () => Math.round(255 * Math.random());
 // FROM SKETCH
 
 const earthTone = [
-	{ hex: '#9B8F77', hsb: 'hsb(40, 23%, 61%)', color: 'gobi' },
-	{ hex: '#9F8B84', hsb: 'hsb(16, 17%, 62%)', color: 'mocha' },
-	{ hex: '#CCBDB6', hsb: 'hsb(19, 11%, 80%)', color: 'dusk' },
-	{ hex: '#CBC2BB', hsb: 'hsb(26, 8%, 80%)', color: 'limestone' },
-	{ hex: '#CCC4B7', hsb: 'hsb(37, 10%, 80%)', color: 'dolomite' },
-	{ hex: '#919596', hsb: 'hsb(192, 3%, 59%)', color: 'shale' },
-	{ hex: '#ACAFB6', hsb: 'hsb(222, 5%, 71%)', color: 'graphite' }
+	{ hex: '#9B8F77', hsb: '40, 23%, 61%', color: 'gobi' },
+	{ hex: '#9F8B84', hsb: '16, 17%, 62%', color: 'mocha' },
+	{ hex: '#CCBDB6', hsb: '19, 11%, 80%', color: 'dusk' },
+	{ hex: '#CBC2BB', hsb: '26, 8%, 80%', color: 'limestone' },
+	{ hex: '#CCC4B7', hsb: '37, 10%, 80%', color: 'dolomite' },
+	{ hex: '#919596', hsb: '192, 3%, 59%', color: 'shale' },
+	{ hex: '#ACAFB6', hsb: '222, 5%, 71%', color: 'graphite' }
 ]
 
 const drawStrokes = strokes => {
 	for (const { creationFrameCount, color: strokeColor, coords, lastSeenFrame, uid } of strokes) {
-		const alpha = lastSeenFrame === frameCount ? 255 : 255 * (1 - ((frameCount - lastSeenFrame) / LIFESPAN));
+		const alpha = lastSeenFrame === frameCount ? 1 : 1 * (1 - ((frameCount - lastSeenFrame) / LIFESPAN));
 		if (alpha <= 0) {
 			if (lastSeenFrame - creationFrameCount > 6 && coords.length > 2) {
 				socket.emit('commitShape', activeStrokes[uid], WIDTH, HEIGHT);
 			}
 			delete (activeStrokes[uid]);
+			console.log('delete', uid, activeStrokes)
 			continue;
 		}
 
@@ -98,12 +99,12 @@ const drawStrokes = strokes => {
 			//translate(coords[i].x, coords[i].y)
 			const xDistance = coords[i].x - coords[i - 1].x
 			const yDistance = coords[i].y - coords[i - 1].y
-
-			tint(color(strokeColor));
-			stroke(color(strokeColor));
+			console.log(`hsba(${strokeColor}, ${alpha})`)
+			//			tint(color(strokeColor));
 			noFill();
 			strokeWeight(5);
 			strokeCap(ROUND);
+			tint(color(`hsba(${strokeColor}, ${alpha})`));
 
 			for (var i = 1; i < coords.length - 1; i++) {
 				image(softBrush, coords[i].x, coords[i].y, brushSize, brushSize);
@@ -126,7 +127,7 @@ const updateActiveStrokes = blobs => {
 		const centreX = halfWidth + adjustedDistanceFromCentre;
 		const centreY = (y + (h / 2)) * HEIGHT;
 
-		const activeStroke = activeStrokes[uid] ?? { chalkCoords: [], creationFrameCount, coords: [], uid, color: earthTone[0].hsb };
+		const activeStroke = activeStrokes[uid] ?? { chalkCoords: [], creationFrameCount, coords: [], uid, color: earthTone[Math.round(earthTone.length * Math.random())].hsb };
 
 		activeStrokes[uid] = {
 			...activeStroke,
